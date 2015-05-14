645a4b09bd89ab827db64ca1794f4739c4c3cd14
askl56 <amscott@openmailbox.org>
2015-05-14 22:03:44 +0100

    fix router/config

--- app/router.js
+++ app/router.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
+        this.resource('products', function() this.resource('product', {
+                path: '/:product_id'));
+        });
 });
-
-export default Router;
