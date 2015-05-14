c85b7b919b37fc8c800cc86dc89f298a1d645d96
askl56 <amscott@openmailbox.org>
2015-05-15 00:40:55 +0100

    further modelling

--- app/models/order.js
+++ app/models/order.js
@@ -1,11 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string')
-  price: DS.attr('decimal')
-  published: DS.attr('boolean')
   user_id: DS.attr('integer')
+  total: DS.attr('decimal')
   created_at: DS.attr('datetime')
   updated_at: DS.attr('datetime')
-  quantity: DS.attr('integer')
 });
